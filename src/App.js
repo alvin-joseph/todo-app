b2ee28c7d65b7b7ad737fb2953c21cf330694e1a
alvin-joseph <aznman1234@gmail.com>
2021-08-26 22:32:33 -0500

    updated style/add name feature

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
+const inputTextValues = {
+  todo: "",
+  name: ""
+}
+
 function App() {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState(inputTextValues);
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+  const [name, setName] = useState("");
 
   useEffect(() => { //this has to be on top because it runs once
     getLocalTodos();
+    getLocalName();
   }, [])
 
   useEffect(() => { //filters an object everytime we add a todo or change status
@@ -36,13 +43,17 @@ function App() {
     const saveLocalTodos = () => {
       localStorage.setItem('todos', JSON.stringify(todos));
     };
+    const saveLocalName = () => {
+      localStorage.setItem('name', JSON.stringify(name));
+    };
 
     filterHandler();
     saveLocalTodos();
-  }, [todos, status])
+    saveLocalName();
+  }, [todos, status, name])
 
   //Save to Local
-    //If our local storage doesn't have todos inside or null then we are going to make an empty array using JSON.stringify, but if it does have something then it will set that item to our state through parse
+    //If our local storage doesn't have todos or name inside(or null) then we are going to make an empty array using JSON.stringify, but if it does have something then it will set that item to our state through parse
     const getLocalTodos = () => {
       if(localStorage.getItem('todos') === null) {
         localStorage.setItem('todos', JSON.stringify([]));
@@ -51,13 +62,22 @@ function App() {
         setTodos(todoLocal);
       }
     };
+    const getLocalName = () => {
+      if(localStorage.getItem('name') === null) {
+        localStorage.setItem('name', JSON.stringify([]));
+      }else {
+        let nameLocal = JSON.parse(localStorage.getItem('name'));
+        setName(nameLocal);
+      }
+    };
     
   return (
     <div className="App">
       <header>
-        <h1>Alvin's Todo List</h1>
+        <h1>{`${name}'s Todo List`}</h1>
       </header>
       <Form 
+      setName={setName}
       setInputText={setInputText} 
       inputText={inputText} 
       setTodos={setTodos} 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,46 @@
 import React from 'react';
 
-const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
+const Form = ({ setInputText, setTodos, todos, inputText, setStatus, setName }) => {
 
     const inputTextHandler = (e) => {
-        //console.log(e.target.value);
-        setInputText(e.target.value);
+        setInputText({
+          ...inputText,
+          todo: e.target.value
+        });
+    };
+
+    const nameInputHandler = (e) => {
+        setInputText({
+          ...inputText,
+          name: e.target.value
+        });
     };
 
     const submitTodoHandler = (e) => {
         e.preventDefault();
         const newTodo = {
-          text: inputText, 
+          text: inputText.todo, 
           completed: false, 
           id: Math.random() * 1000
         }
-        if(inputText.length > 0) {
-          setTodos([
-              ...todos, newTodo
-          ]);
+        if(inputText.todo !== undefined) {
+          if((inputText.todo).length > 0) {
+            setTodos([
+                ...todos, newTodo
+            ]);
+          }
+          setInputText({...inputText, todo:""});
         }
-        setInputText("");
+    }
+
+    const submitNameHandler = e => {
+      e.preventDefault();
+      if(inputText.name !== undefined) {
+        if((inputText.name).length > 0) {
+          setName(inputText.name)
+          setInputText({...inputText, name:""})
+        }
+      }
     }
 
     const statusHandler = e => {
@@ -27,19 +48,27 @@ const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
     }
 
     return(
-    <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
-      <button onClick={submitTodoHandler} className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select onChange={statusHandler} name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
+    <div>
+      <form className="name-form">
+        <input value={inputText.name} onChange={nameInputHandler} type="text" className="name-input" placeholder="Input name"/>
+        <button onClick={submitNameHandler} className="name-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+      </form>
+      <form>
+        <input value={inputText.todo} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
     );
 }
 
